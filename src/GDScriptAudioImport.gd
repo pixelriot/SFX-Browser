079f01d0a0729d326903f548b5c313f3e1c507b7
richi <mail@richardbeyer.org>
2022-06-09 12:05:17 +0200

    improved audio importer.

--- src/GDScriptAudioImport.gd
+++ src/GDScriptAudioImport.gd
@@ -1,7 +1,5 @@
 #GDScriptAudioImport v0.1
 
-#https://github.com/Gianclgar/GDScriptAudioImport
-
 #MIT License
 #
 #Copyright (c) 2020 Gianclgar (Giannino Clemente) gianclgar@gmail.com
@@ -28,86 +26,118 @@
 
 extends Node
 
-const ENABLE_PRINT = false
+func report_errors(err, filepath):
+	# See: https://docs.godotengine.org/en/latest/classes/class_@globalscope.html#enum-globalscope-error
+	var result_hash = {
+		ERR_FILE_NOT_FOUND: "File: not found",
+		ERR_FILE_BAD_DRIVE: "File: Bad drive error",
+		ERR_FILE_BAD_PATH: "File: Bad path error.",
+		ERR_FILE_NO_PERMISSION: "File: No permission error.",
+		ERR_FILE_ALREADY_IN_USE: "File: Already in use error.",
+		ERR_FILE_CANT_OPEN: "File: Can't open error.",
+		ERR_FILE_CANT_WRITE: "File: Can't write error.",
+		ERR_FILE_CANT_READ: "File: Can't read error.",
+		ERR_FILE_UNRECOGNIZED: "File: Unrecognized error.",
+		ERR_FILE_CORRUPT: "File: Corrupt error.",
+		ERR_FILE_MISSING_DEPENDENCIES: "File: Missing dependencies error.",
+		ERR_FILE_EOF: "File: End of file (EOF) error."
+	}
+	if err in result_hash:
+		print("Error: ", result_hash[err], " ", filepath)
+	else:
+		print("Unknown error with file ", filepath, " error code: ", err)
 
 func loadfile(filepath, _loop = false):
 	var file = File.new()
-	file.open(filepath, File.READ)
-	var bytes = file.get_buffer(file.get_len())
+	var err = file.open(filepath, File.READ)
+	if err != OK:
+		report_errors(err, filepath)
+		file.close()
+		return AudioStreamSample.new()
 
+	var bytes = file.get_buffer(file.get_len())
 	# if File is wav
 	if filepath.ends_with(".wav"):
 		var newstream = AudioStreamSample.new()
+
 		#---------------------------
 		#parrrrseeeeee!!! :D
+		print ("---")
+
+		var bits_per_sample = 0
 
 		for i in range(0, 100):
 			var those4bytes = str(char(bytes[i])+char(bytes[i+1])+char(bytes[i+2])+char(bytes[i+3]))
 
 			if those4bytes == "RIFF":
-				_print ("RIFF OK at bytes " + str(i) + "-" + str(i+3))
+				print ("RIFF OK at bytes " + str(i) + "-" + str(i+3))
 				#RIP bytes 4-7 integer for now
+
 			if those4bytes == "WAVE":
-				_print ("WAVE OK at bytes " + str(i) + "-" + str(i+3))
+				print ("WAVE OK at bytes " + str(i) + "-" + str(i+3))
 
 			if those4bytes == "fmt ":
-				_print ("fmt OK at bytes " + str(i) + "-" + str(i+3))
+				print ("fmt OK at bytes " + str(i) + "-" + str(i+3))
 
 				#get format subchunk size, 4 bytes next to "fmt " are an int32
 				var formatsubchunksize = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
-				_print ("Format subchunk size: " + str(formatsubchunksize))
+				print ("Format subchunk size: " + str(formatsubchunksize))
 
 				#using formatsubchunk index so it's easier to understand what's going on
 				var fsc0 = i+8 #fsc0 is byte 8 after start of "fmt "
 
 				#get format code [Bytes 0-1]
 				var format_code = bytes[fsc0] + (bytes[fsc0+1] << 8)
-				var format_name = ""
-				if format_code == AudioStreamSample.FORMAT_8_BITS:
-					format_name = "8_BITS"
-				elif format_code == AudioStreamSample.FORMAT_16_BITS:
-					format_name = "16_BITS"
-				elif format_code == AudioStreamSample.FORMAT_IMA_ADPCM:
-					format_name = "IMA_ADPCM"
+				var format_name
+				if format_code == 0: format_name = "8_BITS"
+				elif format_code == 1: format_name = "16_BITS"
+				elif format_code == 2: format_name = "IMA_ADPCM"
 				else:
-					pass
-				_print ("Format: " + str(format_code) + " " + format_name)
+					format_name = "UNKNOWN (trying to interpret as 16_BITS)"
+					format_code = 1
+				print ("Format: " + str(format_code) + " " + format_name)
 				#assign format to our AudioStreamSample
 				newstream.format = format_code
 
 				#get channel num [Bytes 2-3]
 				var channel_num = bytes[fsc0+2] + (bytes[fsc0+3] << 8)
-				_print ("Number of channels: " + str(channel_num))
+				print ("Number of channels: " + str(channel_num))
 				#set our AudioStreamSample to stereo if needed
 				if channel_num == 2: newstream.stereo = true
 
 				#get sample rate [Bytes 4-7]
 				var sample_rate = bytes[fsc0+4] + (bytes[fsc0+5] << 8) + (bytes[fsc0+6] << 16) + (bytes[fsc0+7] << 24)
-				_print ("Sample rate: " + str(sample_rate))
+				print ("Sample rate: " + str(sample_rate))
 				#set our AudioStreamSample mixrate
 				newstream.mix_rate = sample_rate
 
 				#get byte_rate [Bytes 8-11] because we can
 				var byte_rate = bytes[fsc0+8] + (bytes[fsc0+9] << 8) + (bytes[fsc0+10] << 16) + (bytes[fsc0+11] << 24)
-				_print ("Byte rate: " + str(byte_rate))
+				print ("Byte rate: " + str(byte_rate))
 
 				#same with bits*sample*channel [Bytes 12-13]
 				var bits_sample_channel = bytes[fsc0+12] + (bytes[fsc0+13] << 8)
-				_print ("BitsPerSample * Channel / 8: " + str(bits_sample_channel))
-				#aaaand bits per sample [Bytes 14-15]
-				var bits_per_sample = bytes[fsc0+14] + (bytes[fsc0+15] << 8)
-				_print ("Bits per sample: " + str(bits_per_sample))
+				print ("BitsPerSample * Channel / 8: " + str(bits_sample_channel))
 
+				#aaaand bits per sample/bitrate [Bytes 14-15]
+				bits_per_sample = bytes[fsc0+14] + (bytes[fsc0+15] << 8)
+				print ("Bits per sample: " + str(bits_per_sample))
 
 			if those4bytes == "data":
+				assert(bits_per_sample != 0)
+
 				var audio_data_size = bytes[i+4] + (bytes[i+5] << 8) + (bytes[i+6] << 16) + (bytes[i+7] << 24)
-				_print ("Audio data/stream size is " + str(audio_data_size) + " bytes")
+				print ("Audio data/stream size is " + str(audio_data_size) + " bytes")
 
 				var data_entry_point = (i+8)
-				_print ("Audio data starts at byte " + str(data_entry_point))
+				print ("Audio data starts at byte " + str(data_entry_point))
 
-				newstream.data = bytes.subarray(data_entry_point, data_entry_point+audio_data_size-1)
+				var data = bytes.subarray(data_entry_point, data_entry_point+audio_data_size-1)
 
+				if bits_per_sample in [24, 32]:
+					newstream.data = convert_to_16bit(data, bits_per_sample)
+				else:
+					newstream.data = data
 			# end of parsing
 			#---------------------------
 
@@ -115,7 +145,6 @@ func loadfile(filepath, _loop = false):
 		var samplenum = newstream.data.size() / 4
 		newstream.loop_end = samplenum
 		newstream.loop_mode = 1 if _loop else 0 #change to 0 or delete this line if you don't want loop, also check out modes 2 and 3 in the docs
-
 		return newstream  #:D
 
 	#if file is ogg
@@ -133,14 +162,46 @@ func loadfile(filepath, _loop = false):
 		return newstream
 
 	else:
-		_print ("ERROR: Wrong filetype or format")
+		print ("ERROR: Wrong filetype or format")
 	file.close()
 
-
-
-func _print(output:String) -> void:
-	if ENABLE_PRINT:
-		print(output)
+# Converts .wav data from 24 or 32 bits to 16
+#
+# These conversions are SLOW in GDScript
+# on my one test song, 32 -> 16 was around 3x slower than 24 -> 16
+#
+# I couldn't get threads to help very much
+# They made the 24bit case about 2x faster in my test file
+# And the 32bit case abour 50% slower
+# I don't wanna risk it always being slower on other files
+# And really, the solution would be to handle it in a low-level language
+func convert_to_16bit(data: PoolByteArray, from: int) -> PoolByteArray:
+	print("converting to 16-bit from %d" % from)
+	var time = OS.get_ticks_msec()
+	# 24 bit .wav's are typically stored as integers
+	# so we just grab the 2 most significant bytes and ignore the other
+	if from == 24:
+		var j = 0
+		for i in range(0, data.size(), 3):
+			data[j] = data[i+1]
+			data[j+1] = data[i+2]
+			j += 2
+		data.resize(data.size() * 2 / 3)
+	# 32 bit .wav's are typically stored as floating point numbers
+	# so we need to grab all 4 bytes and interpret them as a float first
+	if from == 32:
+		var spb := StreamPeerBuffer.new()
+		var single_float: float
+		var value: int
+		for i in range(0, data.size(), 4):
+			spb.data_array = data.subarray(i, i+3)
+			single_float = spb.get_float()
+			value = single_float * 32768
+			data[i/2] = value
+			data[i/2+1] = value >> 8
+		data.resize(data.size() / 2)
+	print("Took %f seconds for slow conversion" % ((OS.get_ticks_msec() - time) / 1000.0))
+	return data
 
 
 # ---------- REFERENCE ---------------
